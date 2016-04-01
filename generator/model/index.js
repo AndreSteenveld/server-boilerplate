5ea829377297308b6f5d4c483caec8c0583fe2ae
Andre Steenveld <andre.steenveld@gmail.com>
2016-04-01 11:30:41 +0200

    Another close enough on the model (which is just a schema)

--- generator/model/index.js
+++ generator/model/index.js
@@ -84,7 +84,7 @@ module.exports = generators.Base.extend({
     // Generating the appropriate model based on the orm type.
     this.fs.copyTpl(
       this.templatePath(this.props.type + '.js'),
-      this.destinationPath('src/services/', this.props.service, this.props.name + '-model.js'),
+      this.destinationPath('src/services/', this.props.service, 'schema-' + this.props.name + '.js'),
       this.props
     );
   },
--- generator/model/templates/generic.js
+++ generator/model/templates/generic.js
@@ -1,9 +1,20 @@
-'use strict';
 
-// <%= name %>-model.js - A just a generic object literal model
+export default const <%= name %>Schema = {
+	$schema: "http://json-schema.org/draft-04/schema#",
+	
+	title: "<%= name %>",
+	description:
+		`
+			Schema for <%= name %> objects
+			You can add additional documentation here this might or might not include why the object
+			has been designed this way and constraints that can not be expressed using a schema?
 
-const <%= name %>Model = {
-  text: {type: String, required: true, index: true}
+			For more information about JSON-schemas you might want to visit:
+			- http://json-schema.org/documentation.html
+			- http://spacetelescope.github.io/understanding-json-schema/
+			- http://jsonschema.net/#/
+		`,
+
+	type: "object",
+	properties: { }
 };
-
-module.exports = <%= name %>Model;
